0697db45371de0f659ae37e6739e7d8630f213ed
Gustavo Morais <medeiros@discente.ufg.br>
2024-07-03 19:10:11 -0300

    Movimentação Player

--- Arvendia/Assets/Scripts/Player/PlayerMovement.cs
+++ Arvendia/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,13 +8,18 @@ public class PlayerMovement : MonoBehaviour
     [Header("Config")]
     [SerializeField] private float speed;
 
+    private readonly int moveX = Animator.StringToHash("MoveX");
+    private readonly int moveY = Animator.StringToHash("MoveY");
+
     private PlayerActions actions;
     private Rigidbody2D rb2D;
+    private Animator animator;
     private Vector2 moveDirection;
 
     private void Awake()
     {
         actions = new PlayerActions();
+        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
     
@@ -37,6 +42,9 @@ public class PlayerMovement : MonoBehaviour
     private void ReadMovement()
     {
         moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+        if (moveDirection == Vector2.zero) return;
+        animator.SetFloat(moveX, moveDirection.x);
+        animator.SetFloat(moveY, moveDirection.y);
     }
     
     private void OnEnable()
