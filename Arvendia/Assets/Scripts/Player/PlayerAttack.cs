8fb1797ba41bf48cdbc414d6a82be9c344f6eaab
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2024-07-22 23:42:38 -0300

    Damage Calculation e Critical Chance

--- Arvendia/Assets/Scripts/Player/PlayerAttack.cs
+++ Arvendia/Assets/Scripts/Player/PlayerAttack.cs
@@ -6,6 +6,7 @@ using Random = UnityEngine.Random;
 public class PlayerAttack : MonoBehaviour
 {
     [Header("Config")]
+    [SerializeField] private PlayerStats stats;
     [SerializeField] private Weapon initialWeapon;
     [SerializeField] private Transform[] attackPositions;
 
@@ -79,7 +80,7 @@ public class PlayerAttack : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(new Vector3(0f, 0f, currentAttackRotation));
         Projectile projectile = Instantiate(CurrentWeapon.ProjectilePrefab, currentAttackPosition.position, rotation);
         projectile.Direction = Vector3.up;
-        projectile.Damage = CurrentWeapon.Damage;
+        projectile.Damage = GetAttackDamage();
         playerMana.UseMana(CurrentWeapon.RequiredMana);
     }
 
@@ -91,8 +92,21 @@ public class PlayerAttack : MonoBehaviour
             Vector3.Distance(enemyTarget.transform.position, transform.position);
         if (currentDistanceToEnemy <= minDistanceMeleeAttack)
         {
-            enemyTarget.GetComponent<IDamageable>().TakeDamage(1f);
+            enemyTarget.GetComponent<IDamageable>().TakeDamage(GetAttackDamage());
         }
+    }
+
+    private float GetAttackDamage()
+    {
+        float damage = stats.BaseDamage;
+        damage += CurrentWeapon.Damage;
+        float randomPerc = Random.Range(0f, 100);
+        if (randomPerc <= stats.CriticalChance)
+        {
+            damage += damage * (stats.CriticalDamage / 100f);
+        }
+
+        return damage;
     }
 
     private void GetFirePosition()
